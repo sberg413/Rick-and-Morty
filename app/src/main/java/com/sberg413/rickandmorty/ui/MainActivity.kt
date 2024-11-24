52882286e5250da16e0fc036dfb8278439a8e3bb
Steve Bergamini <stevefb73@yahoo.com>
2024-11-24 08:39:48 -0500

    Enabled edge-to-edge display

--- app/src/main/java/com/sberg413/rickandmorty/ui/MainActivity.kt
+++ app/src/main/java/com/sberg413/rickandmorty/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sberg413.rickandmorty.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 
 import com.sberg413.rickandmorty.ui.theme.AppTheme
 
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             AppTheme {
                 RickMortyApp()
