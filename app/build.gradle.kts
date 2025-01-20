966a5ff49b8798a3f801c05330c5b1e6210b15a1
Steve Bergamini <stevefb73@yahoo.com>
2025-01-20 07:19:19 -0500

    Upgraded Gradle, AGP, compileSdk, targetSdk, Kotlin, KSP, kotlinCompose compiler, and many other dependency version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,12 @@ plugins {
 apply( from = "$rootDir/jacoco.gradle")
 
 android {
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.sberg413.rickandmorty"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
